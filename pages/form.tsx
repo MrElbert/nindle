11f6d5c087d3857aa393bb1a9b1a04949535ce76
Elbert Kim <elbert.kim@icloud.com>
2022-12-20 21:04:54 -0500

    update redirect uri for form

--- pages/form.tsx
+++ pages/form.tsx
@@ -206,7 +206,7 @@ export async function getServerSideProps(context: any) {
   var raw = JSON.stringify({
     grant_type: "authorization_code",
     code: code,
-    redirect_uri: "https://nindle.vercel.app/form",
+    redirect_uri: "https://nindle.xyz/form",
   });
 
   var requestOptions = {
